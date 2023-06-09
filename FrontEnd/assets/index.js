ecc400d58a8ef84b1f5021cfe1ea4360177c0d58
Shifnas <shifnas@ymail.com>
2023-06-09 16:53:39 +0200

    commit : creation de formulaire, login

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,7 +1,7 @@
-	
-const filters = document.querySelector(".filters")
-const gallery = document.querySelector(".gallery")
-
+const filters = document.querySelector(".filters");
+const gallery = document.querySelector(".gallery");
+let works = [];
+let worksFiltered = []
 // const p = document.createElement("p")
 // p.textContent = "text" // content of 'p' is text
 
@@ -9,98 +9,61 @@ const gallery = document.querySelector(".gallery")
 
 // Category
 
-async function fetchCategories(){
-    let response = await fetch("http://localhost:5678/api/categories"); // recuperer les category de base de donner d'API
-    const data = await response.json() // Await = request?
-    const ul = document.createElement("ul")
-    filters.appendChild(ul)
+async function fetchCategories() {
+  let response = await fetch("http://localhost:5678/api/categories"); // recuperer les category de base de donner d'API
+  const data = await response.json(); // Await = request?
+  const ul = document.createElement("ul");
+  filters.appendChild(ul);
+
+  data.map((category) => {
+    // parcourir interiour de tableau (swegger = backend)
+    // console.log(category)
+    // const li = document.createElement("li")
+    // li.textContent = category.name // content de LI
+
+    // ul.appendChild(li)
+    ul.innerHTML += `<li class="filter" data-id=${category.id} >${category.name}</li>`; // class="filter" rajout de classe
+   
+  });
+}
+
+// Work
+async function fetchWorks() {
+  let response = await fetch("http://localhost:5678/api/works");
+  const data = await response.json();
+  works = data; // permet de recuperer en dehor de function
+  displayWorks(works)
+}
+// filters
+async function filterWorks() {
+  const filters = document.querySelectorAll(".filter");
+  filters.forEach((filter) => {
+    filter.addEventListener("click", () => { // fontion pour chaque filtre
+      worksFiltered = works.filter((work) => work.category.id == filter.dataset.id); // filter - work.category.id = une fonction permet de filtrer chaque elements
+      console.log(works,worksFiltered);
+      gallery.innerHTML=''
+     displayWorks(worksFiltered)
+     
     
-    data.map((category ) => { // parcourir interiour de tableau (swegger = backend)
-        // console.log(category)
-        // const li = document.createElement("li")
-        // li.textContent = category.name // content de LI
-
-        // ul.appendChild(li)
-        ul.innerHTML += `<li class="filter" >${category.name}</li>` // class="filter" rajout de classe
-
-    })
-
+        
+    });
+  });
+}
+function displayWorks(data){
+    return  data.map((work) => {
+        const workItem = document.createElement("figure");
+        workItem.innerHTML = `<img src=${work.imageUrl} alt={work.title}>
+    <figcaption>${work.title}</figcaption>`;
+        gallery.appendChild(workItem);
+      });
 }
 
-    // Work
-async function fetchWorks(){
-    let response = await fetch("http://localhost:5678/api/works");
-    const data = await response.json()
-    const p = document.createElement("p")
-    filters.appendChild(p)
-    data.map((works) => {
-        console.log(works)
-    p.innerHTML += `<p classe="titre">${works.title}</li>` 
-    })
-}
-
-
-
-addEventListener("DOMContentLoaded", async (event) => { // le code execute une fois que html est chargé
-    await fetchCategories()
-    await fetchWorks()
+addEventListener("DOMContentLoaded", async (event) => {
+  // le code execute une fois que html est chargé
+  await fetchCategories();
+  await fetchWorks();
+  await filterWorks();
 });
 
 
-// Gallery photos et caption
 
-const imageGallery = [
-    {
-	    "image":"abajour-tahina.png",	
-	},
-    {
-        "image":"appartement-paris-v.png",
-    },
-    {
-	    "image":"appartement-paris-x.png",	
-	},
-    {
-        "image":"appartement-paris-xviii.png",
-    },
-    {
-	    "image":"bar-lullaby-paris.png",	
-	},
-    {
-        "image":"hotel-first-arte-new-delhi.png",
-    },
-    {
-	    "image":"la-balisiere.png",	
-	},
-    {
-        "image":"le-coteau-cassis.png",
-    },
-    {
-        "image":"restaurant-sushisen-londres.png",
-    },
-    {
-	    "image":"sophie-bluel.png",	
-	},
-    {
-        "image":"structures-thermopolis.png",
-    },
-    {
-        "image":"villa-ferneze.png",
-    },
-      
-]
-console.log(imageGallery.length)
-
-const galleryPhoto = document.querySelector(".gallery-photo");
-
-imageGallery.src = "./assets/images"(galleryPhoto);
-document.body.appendChild(imageGallery);
-
-console.log(galleryPhoto);
-
-
-// function displayImage(src) {
-//     let img = document.createElement("img");
-//     img.src = "./assets/images/villa-ferneze.png";
-//     document.body.appendChild(img);
-//    }
-//    displayImage('img');
