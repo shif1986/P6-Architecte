b739ec23391bc173759dcf34142e89d1d3afab59
Shifnas <shifnas@ymail.com>
2023-07-04 15:45:35 +0200

    Modal inserer l'image

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -3,7 +3,15 @@ const gallery = document.querySelector(".gallery");
 let works = [];
 let worksFiltered = [];
 const modalContainer = document.querySelector(".modal");
-const modalAddPhoto = document.querySelector(".Modal-ajouter-photo");
+const modalContainerTwo = document.querySelector(".modal-content2");
+const inputImage = document.querySelector("#image");
+const ajouterPhoto = document.querySelector(".ajouter-photo-2");
+const previewImage = document.querySelector(".preview-image");
+const textJpeg = document.querySelector(".content-image p");
+const formAdd = document.querySelector(".titre-categ");
+
+
+
 
 // const p = document.createElement("p")
 // p.textContent = "text" // content of 'p' is text
@@ -14,7 +22,7 @@ const modalAddPhoto = document.querySelector(".Modal-ajouter-photo");
 
 async function fetchCategories() {
   let response = await fetch("http://localhost:5678/api/categories"); // recuperer les category de base de donner d'API
-  const data = await response.json(); // Await = request?
+  const data = await response.json(); // Await = request? attend la fin de  lexecution promis.()
   const ul = document.createElement("ul");
   filters.appendChild(ul); // recuparation de ul (text)
 
@@ -84,25 +92,26 @@ function check() {
     const headerProjet = document.querySelector(".header-projet");
     headerProjet.appendChild(modif);
   }
-
-  /******* MODAL 1 ********/
-  const openModal = function (e) {
-    modalContainer.style.display = "flex";
-  };
-
-  const closeModal = function () {
-    modalContainer.style.display = "none";
-  };
   // creer un event for class modal trigger pour ouvrir la modal
   document.querySelectorAll(".modal-trigger").forEach((a) => {
     a.addEventListener("click", openModal);
     //const target = document.querySelector(e.target.getAttribute())
   });
-    // on creer un event for class close modal, when click to close the modal
+  // on creer un event for class close modal, when click to close the modal
   document.querySelectorAll(".close-modal").forEach((a) => {
     a.addEventListener("click", closeModal);
+    a.addEventListener("click", closeModalTwo);
   });
 }
+
+/******* MODAL 1 ********/
+const openModal = function (e) {
+  modalContainer.style.display = "flex";
+};
+
+const closeModal = function () {
+  modalContainer.style.display = "none";
+};
 
 // Modal images
 function getImageModal() {
@@ -111,23 +120,31 @@ function getImageModal() {
     // Créer une Variable qui s'appelle Edit qui va nous permettre d'affichier nos titre
     const galleryModal = document.querySelector(".gallery-modal");
     const editModal = document.createElement("figure");
-    const photoEdit = document.createElement("article")
-    
+    const photoEdit = document.createElement("article");
+
     editModal.className = "modal-figure";
-    // On affiche les elements dans le HTML + img
+    // On affiche les elements dans le HTML + img + caption,icons
     editModal.innerHTML = `
             
             <img src=${imageModal.imageUrl} class="modal-img" alt=${imageModal.title}> 
-            <img.title=${imageModal.title} classe="img-edite">éditer
-            <i class="fa-solid fa-trash-can" data-id=${imageModal.id}></i>`
-            ;
-     galleryModal.appendChild(editModal);
+             <span class="image-caption" >éditer</span>
+
+            
+           <p class='trash-box'> <i class="fa-solid fa-trash-can" data-id=${imageModal.id}></i> </p>
+           <p class='move-box'> <i class="fa-solid fa-arrows-up-down-left-right"></i></p>`;
+    galleryModal.appendChild(editModal);
   });
+
+  // breakline
+  const breakLine = document.createElement("hr");
+  breakLine.innerHTML = "<hr>";
+  document.body.appendChild(breakLine);
 }
 // Headers => L'authorization bearer avec le token dedans permet d'envoyer à l'api le fait que tu es connecté avec le token sinon ça ne fonctionnerait pas car l'api vérifie qu'il y
 // a bien ce token pour supprimer ou ajouter un work.
 async function deleteWork() {
-  const deleteButtons = document.querySelectorAll(".modal-icon-delete")
+  const deleteButtons = document
+    .querySelectorAll(".modal-icon-delete")
     .forEach((button) => {
       button.addEventListener("click", async () => {
         const response = await fetch(
@@ -150,22 +167,64 @@ async function deleteWork() {
 }
 
 // Modal 2
-
-const openModalTwo = function(e){
-  modalContainer.style.display = "flex";
+const openModalTwo = function (e) {
+  modalContainerTwo.style.display = "flex";
+  closeModal();
   // document.querySelector(".Modal-ajouter-photo").addEventListener("click", openModalTwo)
-
 };
 
-const closeModalTwo = function (){
-  modalContainer.style.display = "none";
+// Ajouter image
+
+const closeModalTwo = function () {
+  modalContainerTwo.style.display = "none";
 };
 
-document.querySelector(".ajouter-img").addEventListener("click", openModalTwo)
+document.querySelector(".ajouter-img").addEventListener("click", openModalTwo);
+ajouterPhoto.addEventListener("click", () => {
+  inputImage.click();
+});
+inputImage.addEventListener("change", () => {
+  const selectedImage = inputImage.files[0];
+  previewImage.src = URL.createObjectURL(selectedImage);
+  ajouterPhoto.style.display = "none";
+  previewImage.style.width = "auto";
+  previewImage.style.maxHeight = "100%";
+  textJpeg.style.display = "none";
+});
+// fonctionnement de titre et categorie
+console.log(formAdd);
+formAdd.addEventListener("submit", () => {
+  const validateImages = document.querySelector(".valider-image");
 
-// document.querySelectorAll("close-modal").forEach((a) =>{
-//   a.addEventListener("click", closeModal);
-// });
+const title = document.getElementById("photo-title").value;
+const category = document.getElementById("categorie").value;
+const image = document.getElementById("image").files[0];
+
+
+
+  fetch("http://localhost:5678/api/works", {
+    method: "POST",
+    body: JSON.stringify({
+      image:image,
+      title:title,
+      category:category,
+      
+    }),
+
+    headers: {
+      Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+     
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+ 
+  // document.querySelector(".valider-image").addEventListener("click", openModalTwo);
+  // validateImages.addEventListener("click", openModalTwo);
+
+
+  
+});
 
 addEventListener("DOMContentLoaded", async (event) => {
   // le code execute une fois que html est chargé - appel les fonctions
